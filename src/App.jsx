2ab448a341ec2c31b2a54efd49dd11ac51a797a1
Michael <michaeljtp1@gmail.com>
2022-04-11 18:47:26 +0100

    bootstrap icons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import Header from "./components/header";
 import {main,portfolio,resume,testimonials} from "../resumeData.json";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
